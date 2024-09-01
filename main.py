82e0aebce3a0f5d44804be03c6708a681583cf21
Max-Hi <levin.hirsch@web.de>
2024-09-01 23:25:08 +0200

    text generation initial test. API call works, prompt needs improvement.

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_anthropic import ChatAnthropic
+
+
+# Load environment variables from .env file
+load_dotenv()
+
+def generate_text(prompt, model="claude-3-sonnet-20240229", max_tokens=100):
+    llm = ChatAnthropic(anthropic_api_key = os.getenv('ANTHROPIC_API_KEY'), model=model, temperature=0.2, max_tokens=max_tokens)
+    # template = PromptTemplate(input_variables=["prompt"], template="{prompt}")
+    template = ChatPromptTemplate.from_messages([
+        (
+            "system",
+            "You are a storyteller. You are given the beginning of a story and then you complete it.",
+        ),
+        ("human", "{prompt}"),
+        ])
+    chain = template | llm
+    response = chain.invoke({
+                    "prompt": prompt,
+                    })
+    return response
+
+
+
+print(generate_text("One night the lonly wanderer spoke at the fireplace: ", model="claude-3-haiku-20240307"))
